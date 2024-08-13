8e74283da8e1e39657466c2d57441af4fad3b84a
Paul Chesa <paulchesa1@gmail.com>
2024-08-13 05:19:59 +0300

    feat: Added company score in admin portal

--- src/app/shared/services/business.onboarding.scoring.service.ts
+++ src/app/shared/services/business.onboarding.scoring.service.ts
@@ -4,10 +4,12 @@ import { map } from "rxjs";
 import { AuthStateService } from "../../features/auth/services/auth-state.service";
 import {
   BUSINESS_INFORMATION_SUBSECTION_IDS, getInvestorEligibilitySubsectionIds,
+  IMPACT_ASSESMENT_SUBSECTION_IDS,
   INVESTOR_PREPAREDNESS_SUBSECTION_IDS
 } from "../business/services/onboarding.questions.service";
 import { CompanyStateService } from "../../features/organization/services/company-state.service";
 import { MatchedInvestor } from "../interfaces";
+import { GrowthStage } from "../../features/organization/interfaces";
 
 @Injectable({
   providedIn: 'root'
@@ -18,19 +20,27 @@ export class BusinessOnboardingScoringService {
   private _scoringService = inject(BusinessAndInvestorMatchingService)
   private _companyService = inject(CompanyStateService)
   private _userId = this._authStateService.currentUserId() && this._authStateService.currentUserId() > 0 ? this._authStateService.currentUserId() : Number(sessionStorage.getItem('userId'));
-  getOnboardingScores() {
-    const INVESTOR_ELIGIBILITY = getInvestorEligibilitySubsectionIds(this._companyService.currentCompany.growthStage);
-    return this._scoringService.getOnboardingScores(this._userId).pipe(map(scores => {
+  getOnboardingScores(companyGrowthStage?: GrowthStage, userId = this._userId) {
+    const INVESTOR_ELIGIBILITY = getInvestorEligibilitySubsectionIds(companyGrowthStage ?? this._companyService.currentCompany.growthStage);
+
+    return this._scoringService.getOnboardingScores(userId).pipe(map(scores => {
+
       const businessFinancialsKeys = [...Object.values(BUSINESS_INFORMATION_SUBSECTION_IDS)].filter(key => key !== BUSINESS_INFORMATION_SUBSECTION_IDS.ID);
       const investorEligibilityKeys = [...Object.values(INVESTOR_ELIGIBILITY)].filter(key => key !== INVESTOR_ELIGIBILITY.ID);
       const investorPreparednessKeys = [...Object.values(INVESTOR_PREPAREDNESS_SUBSECTION_IDS)].filter(key => key !== INVESTOR_PREPAREDNESS_SUBSECTION_IDS.ID);
+      const impactAssessmentKeys = [...Object.values(IMPACT_ASSESMENT_SUBSECTION_IDS)].filter(key => key !== IMPACT_ASSESMENT_SUBSECTION_IDS.ID)
+
       const businessFinancialScore = scores.filter((score) => businessFinancialsKeys.indexOf(score.subSectionId ?? -1) > -1).map(score => score.percentageScore)
       const investorEligibilityScore = scores.filter((score) => investorEligibilityKeys.indexOf(score.subSectionId ?? -1) > -1).map(score => score.percentageScore)
       const investorPreparednessScore = scores.filter((score) => investorPreparednessKeys.indexOf(score.subSectionId ?? -1) > -1).map(score => score.percentageScore)
+      const impactAssessmentScore = scores.filter((score) => impactAssessmentKeys.indexOf(score.subSectionId ?? -1) > -1).map(score => score.percentageScore)
+
+
       return {
         businessFinancials: Number(Math.ceil(businessFinancialScore.reduce((prev, acc) => prev + acc, 0) / businessFinancialScore.length)).toFixed(1),
         investorEligibility: Number(Math.ceil(investorEligibilityScore.reduce((prev, acc) => prev + acc, 0) / investorEligibilityScore.length)).toFixed(1),
         investorPreparedness: Number(Math.ceil(investorPreparednessScore.reduce((prev, acc) => prev + acc, 0) / investorPreparednessScore.length)).toFixed(1),
+        impactAssessment: Number(Math.ceil(impactAssessmentScore.reduce((prev, acc) => prev + acc, 0) / impactAssessmentScore.length)).toFixed(1),
       }
     }))
   }
@@ -45,7 +55,7 @@ export class BusinessOnboardingScoringService {
     }))
   }
 
-  getGeneralSummary(score:number, type:string) {
+  getGeneralSummary(score: number, type: string) {
     return this._scoringService.getGeneralSummary(score, type).pipe(map(generalSummary => {
       return generalSummary
     }))
