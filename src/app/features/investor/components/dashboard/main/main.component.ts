40c6c601b92a69c5d52950a81073ce031a0e1880
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-08-20 11:27:29 +0300

    chore: added phone add banner to investor

--- src/app/features/investor/components/dashboard/main/main.component.ts
+++ src/app/features/investor/components/dashboard/main/main.component.ts
@@ -19,8 +19,13 @@ import { MatchedBusiness } from '../../../../../shared/interfaces';
 import { inject } from '@angular/core';
 import { BusinessAndInvestorMatchingService } from '../../../../../shared/business/services/busines.and.investor.matching.service';
 import { AuthStateService } from '../../../../auth/services/auth-state.service';
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { AlertComponent } from "../../../../../shared/components/alert/alert.component";
+import { SignalsService } from '../../../../../core/services/signals/signals.service';
+import { MobileNumber, UserMobileNumbersIssues } from '../../../../auth/interfaces/auth.interface';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DialogModule } from 'primeng/dialog';
 
 
 @Component({
@@ -35,13 +40,15 @@ import { CommonModule } from '@angular/common';
     SchedulesSectionComponent,
     OverviewComponent,
     CardComponent,
-    CommonModule
-  ],
+    CommonModule,
+    AlertComponent,
+    DialogModule,
+    ReactiveFormsModule,
+],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
-  private _authService = inject(AuthStateService)
   private _businessMatchingService = inject(BusinessAndInvestorMatchingService)
   visible = false;
   matchedBusinesses: MatchedBusiness[] = []
@@ -52,6 +59,50 @@ export class MainComponent {
 
   showDialog() {
     this.visible = true;
+  }
+
+
+  private _authStateService = inject(AuthStateService);
+  issue =UserMobileNumbersIssues;
+  signalsService =inject(SignalsService);
+  private _fb =inject(FormBuilder);
+  savephoneNumber$ =new Observable<any>();
+  phoneNumberPull$ =new Observable<any>();
+
+  phoneUpdateForm =this._fb.group({
+    field: ['', [
+      Validators.required,
+    ]]
+  })
+
+  showalert(){
+    this.signalsService.showDialog.set(true)
+  }
+
+  savePhoneNumber(){
+    const field =(this.phoneUpdateForm.value?.field)??'';
+    if(this.signalsService.actionBody().issue ==this.issue.EMPTY){
+      this.savephoneNumber$ =this._authStateService.saveUserPhoneNumberAddedStatus(field).pipe(tap(res =>{
+        return res
+      }))
+      
+    } else if(this.signalsService.actionBody().issue ==this.issue.UNVERIFIED){
+      const mobileNumbers:MobileNumber[] =JSON.parse(sessionStorage.getItem('mobile_numbers')??JSON.stringify([]))
+      if(mobileNumbers.length){
+        this.savephoneNumber$ =this._authStateService.verifyPhoneNumber(field, mobileNumbers.at(0)?.phoneNo??'').pipe(tap(res =>{
+          return res
+        }))
+
+      }
+    }
+  }
+
+  ngOnChanges(): void {
+    this.phoneNumberPull$ =this._authStateService.checkPhoneNumberStatus().pipe(tap((res: UserMobileNumbersIssues) =>{
+      this.signalsService.showInAppAlert.set(res !==UserMobileNumbersIssues.VERIFIED);
+      if(res ===UserMobileNumbersIssues.UNVERIFIED)
+        this.signalsService.actionBody.set({issue: UserMobileNumbersIssues.UNVERIFIED, command: 'Verify', message: 'Please Verify your phone number', title: 'Action Required'})
+    }));
   }
 
 
