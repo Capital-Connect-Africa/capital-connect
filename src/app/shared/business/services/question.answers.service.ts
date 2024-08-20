76389d19f40e4512eb30efefc4d7dec3aa7a4b7d
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-08-21 00:14:08 +0300

    chore: added functionality to search the draft state

--- src/app/shared/business/services/question.answers.service.ts
+++ src/app/shared/business/services/question.answers.service.ts
@@ -1,31 +1,49 @@
-import { map } from "rxjs";
+import { map, of } from "rxjs";
 import { inject, Injectable } from "@angular/core";
 import { Question } from "../../../features/questions/interfaces";
 import { SubMissionStateService } from "./submission-state.service";
-import { UserSubmissionResponse } from "../../interfaces/submission.interface";
+import { Submission, UserSubmissionResponse } from "../../interfaces/submission.interface";
+import { UserSubmissionsService } from "../../../core/services/storage/user-submissions.service";
 
 @Injectable({providedIn: 'root'})
 
 export class QuestionsAnswerService{
     private _submissionStateService =inject(SubMissionStateService);
+    private _userSubmissionsService =inject(UserSubmissionsService);
 
-    private _searchQuestions(submissionResponse:UserSubmissionResponse[], questions: Question[]){
-        const questionsSubmitted:Question[] =[];
+    private _searchAnswersFromSubmissions(submissionResponse:UserSubmissionResponse[], questions: Question[]){
+        const questionsSubmitted:Question[] =questions;
         submissionResponse.forEach(submission =>{
-            questions.forEach(q =>{
-                if(q.id ===submission.question.id) questionsSubmitted.push({
+            questions.forEach((q, index) =>{
+                if(q.id ===submission.question.id) questionsSubmitted[index] ={
                     ...q,
                     submissionId: submission.id,
                     defaultValues:[ { answerId: submission.answer.id, text: submission.answer.text } ]
-                })
+                }
+            })
+        })
+        return questionsSubmitted;
+    }
+
+    private _searchAnswersFromDrafts(draft:Submission[], questions:Question[]){
+        const questionsSubmitted:Question[] =questions;
+        draft.forEach(submission =>{
+            questions.forEach((q, index) =>{
+                if(q.id ===submission.questionId) questionsSubmitted[index] ={
+                    ...q,
+                    submissionId: submission.id,
+                    defaultValues:[ { answerId: submission.answerId, text: submission.text } ]
+                }
             })
         })
         return questionsSubmitted;
     }
 
     investorEligibilityQuestionsAnswers(questions:Question[]) {
+        const draft =this._userSubmissionsService.investorEligibilityDraft.flat();
+        if(draft.length) return of(this._searchAnswersFromDrafts(draft, questions));
         return this._submissionStateService.getSectionSubmissions().pipe(map(res =>{
-            return this._searchQuestions(res?.investor_eligibility??[], questions);
+            return this._searchAnswersFromSubmissions(res?.investor_eligibility??[], questions);
         }))
     }
 }
