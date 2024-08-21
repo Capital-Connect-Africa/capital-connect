63470d6d1e3a3a203d6a2bfd319b8ac42e61a9fa
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-08-21 11:53:46 +0300

    chore: added functionality to save submission progress

--- src/app/core/services/storage/user-submissions.service.ts
+++ src/app/core/services/storage/user-submissions.service.ts
@@ -16,4 +16,28 @@ export class UserSubmissionsService {
   businessInformationDraft:Submission[][] =[];
   investorPreparednessDraft:Submission[][] =[];
 
+
+  saveBusinessInformationSubmissionProgress(submissions:Submission[], step =0){
+    const submissionsCount =this.businessInformationSubmissions.length;
+    if(submissionsCount <=step) this.businessInformationSubmissions.push(submissions);
+    else this.businessInformationSubmissions[step] =submissions;
+  }
+
+  saveInvestorEligibilitySubmissionProgress(submissions:Submission[], step =0){
+    const submissionsCount =this.investorEligibilitySubmissions.length;
+    if(submissionsCount <=step) this.investorEligibilitySubmissions.push(submissions);
+    else this.investorEligibilitySubmissions[step] =submissions;
+  }
+
+  saveInvestorPreparednessSubmissionProgress(submissions:Submission[], step =0){
+    const submissionsCount =this.investorPreparednessSubmissions.length;
+    if(submissionsCount <=step) this.investorPreparednessSubmissions.push(submissions);
+    else this.investorPreparednessSubmissions[step] =submissions;
+  }
+
+  saveImpactAssessmentSubmissionProgress(submissions:Submission[], step =0){
+    const submissionsCount =this.impactAssessmentSubmissions.length;
+    if(submissionsCount <=step) this.impactAssessmentSubmissions.push(submissions);
+    else this.impactAssessmentSubmissions[step] =submissions;
+  }
 }
