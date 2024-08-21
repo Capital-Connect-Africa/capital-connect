6413c139027dd02a2b2350a331fd9881867bf6b2
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-08-22 00:50:48 +0300

    chore: fetch choices

--- src/app/features/business/components/onboarding-form/onboarding-form.component.ts
+++ src/app/features/business/components/onboarding-form/onboarding-form.component.ts
@@ -1,16 +1,25 @@
 import { Router } from '@angular/router';
 import { Component, inject } from '@angular/core';
 import { MultiSelectModule } from 'primeng/multiselect';
+import { BusinessOnboardingService } from '../../services/business-profile/business.onboarding.service';
+import { tap } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-onboarding-form',
   standalone: true,
-  imports: [MultiSelectModule],
+  imports: [CommonModule, MultiSelectModule],
   templateUrl: './onboarding-form.component.html',
   styleUrl: './onboarding-form.component.scss'
 })
 export class OnboardingFormComponent {
-  private _router =inject(Router)
+  private _router =inject(Router);
+  private _businessOnboardingService =inject(BusinessOnboardingService);
+
+  choices$ =this._businessOnboardingService.getQuestionAnswers().pipe(tap(res =>{
+    debugger
+    return res
+  }))
 
   goToDashboard(){
     this._router.navigateByUrl('/business')
