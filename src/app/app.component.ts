891e1a0952e347cba48b1554457f9c96eec25e3c
Paul Chesa <paulchesa1@gmail.com>
2024-08-21 21:13:13 +0300

    chore> Test rebuild staging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,8 @@ export class AppComponent implements OnInit, OnDestroy {
     this._trackLoadingStatusSubscription();
   }
 
+
+  //Subscription
   private _trackLoadingStatusSubscription() {
     return this._loadingService.loading$.pipe(distinctUntilChanged()).subscribe(res => {
       this.isLoading = res;
