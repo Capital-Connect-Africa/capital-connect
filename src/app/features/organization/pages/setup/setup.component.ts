6345bfb50a92fb2f7400395d4358066684be18c4
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-08-22 09:30:35 +0300

    chore: route to the business profile page

--- src/app/features/organization/pages/setup/setup.component.ts
+++ src/app/features/organization/pages/setup/setup.component.ts
@@ -51,7 +51,7 @@ export class SetupComponent implements OnInit {
       this.companyOfUser$ = this._organizationOnboardService.getCompanyOfUser().pipe(filter(() => !this.isEditMode), tap(company => {
         if (company && company.id) {
           this._cd.detectChanges()
-          this.goToInvestorEligibility();
+          this.goToBusinessProfile();
         }
       }));
     }
@@ -83,8 +83,8 @@ export class SetupComponent implements OnInit {
     return true;
   }
 
-  goToInvestorEligibility() {
-    this._router.navigateByUrl('/business/investor-eligibility')
+  goToBusinessProfile() {
+    this._router.navigateByUrl('/business/profile')
   }
   goToDashBoard() {
     this._router.navigateByUrl('/business')
