8e2a8fce132f75ec80571b9c498f7c1843bb2c28
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-08-27 00:07:58 +0300

    chore: added a signals profile

--- src/app/core/services/signals/signals.service.ts
+++ src/app/core/services/signals/signals.service.ts
@@ -18,6 +18,7 @@ export class SignalsService {
   pageTitle: WritableSignal<string> =signal('Dashboard')
   userSectionSubmissions: WritableSignal<SectionSubmissions | null> =signal(null);
   matchedInvestorsDialogIsVisible: WritableSignal<boolean> =signal(false);
-  connectedInvestorsDialogIsVisible: WritableSignal<boolean> =signal(false)
+  connectedInvestorsDialogIsVisible: WritableSignal<boolean> =signal(false);
+  declinedConnectionsDialogIsVisible: WritableSignal<boolean> =signal(false)
 
 }
