58df0e23f202e1c629ba528cc665b3cc4e11c6e7
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-08-29 01:20:23 +0300

    chore: added route to special criteria

--- src/app/features/business/modules/my.business.routing.ts
+++ src/app/features/business/modules/my.business.routing.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadComponent: () => import('../pages/my-business/my-business.component').then(c => c.MyBusinessComponent) },
   { path: 'investors/:id', loadComponent: () => import('../pages/investor-details/investor-details.component').then(c => c.InvestorDetailsComponent) },
+  { path: 'special-criteria', loadComponent: () => import('../pages/special-criteria/special-criteria.component').then(c => c.SpecialCriteriaComponent) },
   
 ]
 @NgModule({
