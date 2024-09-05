09abe30a7ca791b4b82ea158f58a9095370d55d4
j-netcom-dev <netcomdeveloper099@gmail.com>
2024-09-05 12:16:10 +0300

    chore: reload company

--- src/app/features/profile/components/main/main/main.component.ts
+++ src/app/features/profile/components/main/main/main.component.ts
@@ -9,10 +9,11 @@ import { CommonModule } from "@angular/common";
 import { SignalsService } from '../../../../../core/services/signals/signals.service';
 import { AlertComponent } from "../../../../../shared/components/alert/alert.component";
 import { ProfileService } from '../../../services/profile.service';
-import { Observable, tap } from 'rxjs';
+import { forkJoin, map, Observable, switchMap, tap } from 'rxjs';
 import { RoutingService } from '../../../../../shared/business/services/routing.service';
 import { AlertCardComponent } from "../../alert-card/alert-card.component";
 import { Router } from '@angular/router';
+import { OrganizationOnboardService } from '../../../../organization/services/organization-onboard.service';
 
 @Component({
   selector: 'app-main',
@@ -33,7 +34,8 @@ export class MainComponent {
 @Input() showBanner =false;
   private _profileService =inject(ProfileService);
   private _routingService =inject(RoutingService)
-  private _router =inject(Router)
+  private _router =inject(Router);
+  private _companyOnboardingService =inject(OrganizationOnboardService);
   submissions:{title?: string, url: string}[] =[];
 
   submissionActions$ =this._routingService.nextRoute().pipe(tap(res =>{
@@ -41,9 +43,14 @@ export class MainComponent {
     this.submissions =res.filter(r =>r.url !=='/business')
   }))
   
+  company$ =new Observable<any>()
+ 
 
-  userProfile$ =this._profileService.get().pipe(tap(res =>{
-    return res;
+  userProfile$ =forkJoin([this._profileService.get(), this._companyOnboardingService.getCompanyOfUser()]).pipe(map(res =>{
+    return {profile: res[0], company: res[1]}
+  }),
+  tap(res =>{
+    return res
   }))
   signalsService =inject(SignalsService);
   showDialog(){
