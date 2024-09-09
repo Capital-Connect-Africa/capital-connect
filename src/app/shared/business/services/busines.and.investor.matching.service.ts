2250142de79010097b05c2887fdf34c7d1bad945
Trevor-crypto243 <otitrevor450@gmail.com>
2024-09-09 13:31:12 +0300

    Resolving the error on decline

--- src/app/shared/business/services/busines.and.investor.matching.service.ts
+++ src/app/shared/business/services/busines.and.investor.matching.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { map, Observable, tap } from "rxjs";
 import { BASE_URL, BaseHttpService } from "../../../core";
 import { Score } from "./onboarding.questions.service";
-import {MatchedBusiness, MatchedInvestor,InterestingBusinesses,ConnectedBusiness, MatchMakingStats} from "../../interfaces";
+import {MatchedBusiness, MatchedInvestor,InterestingBusinesses,ConnectedBusiness, MatchMakingStats, ConnectionRequest, ConnectionRequestBody} from "../../interfaces";
 import { GeneralSummary, UserSubmissionResponse } from "../../interfaces/submission.interface";
 import { Submission } from "../../interfaces/submission.interface";
 import { Company } from "../../../features/organization/interfaces";
@@ -118,8 +118,6 @@ export class BusinessAndInvestorMatchingService extends BaseHttpService {
   }
 
   //search a company
-  // ### Search companies based on investor profile criteria and text prod (investorProfileId, status, query)
-  // GET https://capitalconnect-0060e0fb0eb4.herokuapp.com/matchmaking/search-matches/35?status=interesting&q=kenya
   searchCompany(status:string, query:string):Observable<InterestingBusinesses[]>{
     let investorProfileId = Number(sessionStorage.getItem('profileId'))
     return this.read(`${BASE_URL}/matchmaking/search-matches/${investorProfileId}?status=${status}&q=${query}`).pipe(
@@ -136,13 +134,12 @@ export class BusinessAndInvestorMatchingService extends BaseHttpService {
   }
 
 
+
+
+
   //Interesting companies
   getInterestingCompanies(page:number, limit:number): Observable<InterestingBusinesses[]> {
     let investorProfileId = Number(sessionStorage.getItem('profileId'))
-    // return this.read(`${BASE_URL}/matchmaking/interested/${investorProfileId}?limit=${limit}`).pipe(
-    //   map(res => res as InterestingBusinesses[])
-    // );
-
     return this.read(`${BASE_URL}/matchmaking/interested/${investorProfileId}?page=${page}&limit=${limit}`).pipe(
       map(res => res as InterestingBusinesses[])
     );
@@ -173,12 +170,44 @@ export class BusinessAndInvestorMatchingService extends BaseHttpService {
     );
   }
 
+  //create a connection request
+  createAConnectionRequest(body: ConnectionRequestBody): Observable<void> {
+    let investorProfileId = Number(sessionStorage.getItem('profileId'))
+    return this.create(`${BASE_URL}/connection-requests`, body).pipe(
+      map(() => void 0) 
+    );
+  }
+  //Get connection request by Id
+  getConnectionRequestById(id:number){
+    return this.readById(`${BASE_URL}/connection-requests/`, id).pipe(map(res =>{
+      return res as ConnectionRequest;
+    }))
+  }
+  //get All connection requests made by an investor
+  getConnectionRequestByInvestor(){
+    let investorProfileId = Number(sessionStorage.getItem('profileId'))
+    return this.readById(`${BASE_URL}/connection-requests/investor/`, investorProfileId).pipe(map(res =>{
+      return res as ConnectionRequest;
+    }))
+  }
+  //Update connection request
+  //create a connection request
+  updateConnectionRequest(body: ConnectionRequestBody): Observable<void> {
+    let investorProfileId = Number(sessionStorage.getItem('profileId'))
+    return this.create(`${BASE_URL}/connection-requests`, body).pipe(
+      map(() => void 0) 
+    );
+  }
+  //Delete connection request
+  //Approve connection request
+  //Decline connection request
+
 
   //Download CSV
   matchMakingCsv(status:string){
     let investorProfileId = Number(sessionStorage.getItem('profileId'))
     return this.downloadExcel(`${BASE_URL}/matchmaking/download-csv`,investorProfileId,status).pipe(tap(blob=>{
-      const fileName = `matchmaking_${status}.csv`; // Use the .xlsx extension for Excel files
+      const fileName = `matchmaking_${status}.csv`; 
       this.saveFile(blob, fileName);      
     })
 
