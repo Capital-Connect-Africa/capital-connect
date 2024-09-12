cb4f5543877283151946e0006022d29e3c2db45c
meltus-bwire <meltusbwire10@gmail.com>
2024-09-12 16:18:31 +0300

    chore: removed approve and decline route to a standalone page

--- src/app/features/business/modules/my.business.routing.ts
+++ src/app/features/business/modules/my.business.routing.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 const routes: Routes = [
   { path: '', loadComponent: () => import('../pages/my-business/my-business.component').then(c => c.MyBusinessComponent) },
   { path: 'investors/:id', loadComponent: () => import('../pages/investor-details/investor-details.component').then(c => c.InvestorDetailsComponent) },
   { path: 'special-criteria/:id', loadComponent: () => import('../pages/special-criteria/special-criteria.component').then(c => c.SpecialCriteriaComponent) },
-  { path: 'connection-requests/:uuid/:action', loadComponent: () => import('../pages/connection-requests/connection-requests.component').then(c => c.ConnectionRequestsComponent) },
   
 ]
 @NgModule({
