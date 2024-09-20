e04d9b04b33e007726c03a5ba1f7e5affbe51437
meltus-bwire <meltusbwire10@gmail.com>
2024-09-20 11:36:36 +0300

    refactor: cleaned ui elements

--- src/app/features/business/pages/dashboard/dashboard.component.ts
+++ src/app/features/business/pages/dashboard/dashboard.component.ts
@@ -14,7 +14,7 @@ export class DashboardComponent {
   links =[
     {label: 'Dashboard', href: '/business', exact: true, icon: 'grid_view'},
     {label: 'My Business', href: '/business/my-business', exact: false, icon: 'business_center'},
-    {label: 'My Bookings', href: '/business/my-bookings', exact: false, icon: 'event'},
+    // {label: 'My Bookings', href: '/business/my-bookings', exact: false, icon: 'event'},
     {label: 'My Profile', href: '/user-profile', exact: true, icon: 'person'},
 
   ]
