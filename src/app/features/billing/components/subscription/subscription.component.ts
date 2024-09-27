d5e894a294be47de6dd22e76fd3aca7a708b8464
meltus-bwire <meltusbwire10@gmail.com>
2024-09-27 10:48:31 +0300

    chore: set basic plan

--- src/app/features/billing/components/subscription/subscription.component.ts
+++ src/app/features/billing/components/subscription/subscription.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { BillingService } from '../../services/billing.service';
 import { CommonModule } from '@angular/common';
-import { Observable, tap } from 'rxjs';
+import { catchError, EMPTY, Observable, switchMap, tap } from 'rxjs';
 import { SubscriptionResponse, SubscriptionTier } from '../../../../shared/interfaces/Billing';
 import { NumberAbbriviationPipe } from '../../../../core/pipes/number-abbreviation.pipe';
 import { DialogModule } from 'primeng/dialog';
@@ -25,13 +25,22 @@ export class SubscriptionComponent {
   private _sanitizer =inject(DomSanitizer);
   private _billingService =inject(BillingService);
   subscribe$ =new Observable<SubscriptionResponse>();
-  plan:string ='';
-  subscriptionTiers$ =this._billingService.getSubscriptionTiers().pipe(tap(res =>{
+  plan:string ='';;
+  activePlan$ =new Observable()
+  subscriptionTiers$ =this._billingService.getSubscriptionTiers().pipe(switchMap(res =>{
     this.tiers =res;
+    return this._billingService.getActivePlan().pipe(tap(res =>{
+      this.activePlan =res;
+      debugger
+    }),
+    catchError(err =>{
+      debugger
+      this.activePlan =this.tiers.find((tier:SubscriptionTier) =>tier.price ==0) as SubscriptionTier;
+      return EMPTY;
+    })
+  )
   }))
-  activePlan$ =this._billingService.getActivePlan().pipe(tap(res =>{
-    this.activePlan =res;
-  }))
+  
 
   subscribe(tierId: number){
     this.plan =this.tiers.find((tier: SubscriptionTier) =>tier.id ===tierId)?.name as string;
