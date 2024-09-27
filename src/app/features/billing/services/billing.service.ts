309d6a3ff9ef78e986f2ef4d3e1562e9780a7f21
meltus-bwire <meltusbwire10@gmail.com>
2024-09-27 11:33:20 +0300

    fix: fixed cyclic deps issue

--- src/app/features/billing/services/billing.service.ts
+++ src/app/features/billing/services/billing.service.ts
@@ -7,30 +7,28 @@ import { AuthStateService } from "../../auth/services/auth-state.service";
 
 @Injectable({providedIn: 'root'})
 
-export class BillingService extends BaseHttpService{
+export class BillingService {
     private _authStateService =inject(AuthStateService);
-    constructor(private _httpClient: HttpClient) {
-        super(_httpClient);
-      }
+    private __http =inject(BaseHttpService);
 
     //Create  a subscription tier
     createSubscriptionTier(subscriptionTier:SubscriptionTier){
-        return this.create(`${BASE_URL}/subscription-tiers`,subscriptionTier).pipe(
+        return this.__http.create(`${BASE_URL}/subscription-tiers`,subscriptionTier).pipe(
             map((res=>{}))
         )
     }
 
     //Get all Subscription Tiers
     getSubscriptionTiers():Observable<SubscriptionTier[]> {
-        return this.read(`${BASE_URL}/subscription-tiers`).pipe(map((res:any) => {
+        return this.__http.read(`${BASE_URL}/subscription-tiers`).pipe(map((res:any) => {
             const tiers:SubscriptionTier[] =res
             return tiers.sort((a:SubscriptionTier, b: SubscriptionTier) =>a.price - b.price)
         })) as Observable<SubscriptionTier[]>
     }
-    
+
     //Get a single subscription tier
     getSubscriptionTier(id:number):Observable<SubscriptionTier> {
-        return this.read(`${BASE_URL}/subscription-tiers`).pipe(map(
+        return this.__http.read(`${BASE_URL}/subscription-tiers`).pipe(map(
             res => res as unknown as SubscriptionTier
         ))
     }
@@ -38,27 +36,27 @@ export class BillingService extends BaseHttpService{
 
     //Update a subscription tier
     updateSubscriptionTier(subscriptionTier:SubscriptionTier,id:number){
-        return this.update(`${BASE_URL}/subscription-tiers`,id,subscriptionTier).pipe(
+        return this.__http.update(`${BASE_URL}/subscription-tiers`,id,subscriptionTier).pipe(
             map((res=>{}))
         )
     }
     
     //Delete a subscription tier
     deleteTier(id:number){
-        return this.delete(`${BASE_URL}/subscription-tiers`,id).pipe(
+        return this.__http.delete(`${BASE_URL}/subscription-tiers`,id).pipe(
             map((res=>{}))
         )
     }
 
     subscribe(tierId: number){
-        return this.create(`${BASE_URL}/subscriptions/subscribe`, {subscriptionTierId: tierId}).pipe(map((res: any) =>{
+        return this.__http.create(`${BASE_URL}/subscriptions/subscribe`, {subscriptionTierId: tierId}).pipe(map((res: any) =>{
             return res;
         })) as Observable<SubscriptionResponse>
     }
 
     getActivePlan(){
         const userId =this._authStateService.currentUserId()
-        return this.read(`${BASE_URL}/subscriptions/${userId}`).pipe(map((res: any) =>{
+        return this.__http.read(`${BASE_URL}/subscriptions/${userId}`).pipe(map((res: any) =>{
             return res;
         })) as Observable<SubscriptionTier>
     }
