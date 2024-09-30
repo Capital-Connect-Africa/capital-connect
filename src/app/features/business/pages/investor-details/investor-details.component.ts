fe8a6f1e410298b860257d25a175dd53da5cdea2
meltus-bwire <meltusbwire10@gmail.com>
2024-09-30 09:27:49 +0300

    chore: added routes to plan and special criteria

--- src/app/features/business/pages/investor-details/investor-details.component.ts
+++ src/app/features/business/pages/investor-details/investor-details.component.ts
@@ -24,7 +24,9 @@ export class InvestorDetailsComponent {
   private _signalService =inject(SignalsService);
   links =[
     {label: 'Dashboard', href: '/business', exact: true, icon: 'grid_view'},
+    {label: 'Plans', href: '/business/plans', exact: false, icon: 'paid'},
     {label: 'My Business', href: '/business/my-business', exact: false, icon: 'business_center'},
+    {label: 'Special Criteria', href: '/business/special-criteria', exact: false, icon: 'contact_support'},
     // {label: 'My Bookings', href: '/business/my-bookings', exact: false, icon: 'event'},
     {label: 'My Profile', href: '/user-profile', exact: true, icon: 'person'},
 
