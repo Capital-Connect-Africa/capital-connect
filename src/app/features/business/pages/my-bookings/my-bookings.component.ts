b4d30d99f73f35f25a8af05c4f193b6a7b9e3425
meltus-bwire <meltusbwire10@gmail.com>
2024-10-02 14:42:14 +0300

    refactor: integrate the reusable business links listing

--- src/app/features/business/pages/my-bookings/my-bookings.component.ts
+++ src/app/features/business/pages/my-bookings/my-bookings.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {SidenavComponent} from "../../../../core";
 import {BookingComponent} from "../../components/my-bookings/main/booking.component";
+import { BusinessLinks } from '../../../../core/utils/business.links';
 
 @Component({
   selector: 'app-my-bookings',
@@ -13,13 +14,5 @@ import {BookingComponent} from "../../components/my-bookings/main/booking.compon
   styleUrl: './my-bookings.component.scss'
 })
 export class MyBookingsComponent {
-  links =[
-    {label: 'Dashboard', href: '/business', exact: true, icon: 'grid_view'},
-    {label: 'Plans', href: '/business/plans', exact: false, icon: 'paid'},
-    {label: 'My Business', href: '/business/my-business', exact: false, icon: 'business_center'},
-    {label: 'Special Criteria', href: '/business/special-criteria', exact: false, icon: 'contact_support'},
-    {label: 'My Bookings', href: '/business/my-bookings', exact: false, icon: 'event'},
-    {label: 'My Profile', href: '/user-profile', exact: true, icon: 'person'},
-
-  ]
+  links =BusinessLinks
 }
