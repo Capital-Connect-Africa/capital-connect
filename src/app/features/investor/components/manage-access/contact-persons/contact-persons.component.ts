7374dfa83ef2490c643b9bfe087ab5f5a7b14e36
meltus-bwire <meltusbwire10@gmail.com>
2024-10-29 00:25:54 +0300

    refactor: modified the logic of the contact persons

--- src/app/features/investor/components/manage-access/contact-persons/contact-persons.component.ts
+++ src/app/features/investor/components/manage-access/contact-persons/contact-persons.component.ts
@@ -2,6 +2,7 @@ import { Observable, tap } from 'rxjs';
 import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { NameInitialsPipe } from '../../../../../core/pipes/name-initials.pipe';
 import { InvestorScreensService } from '../../../services/investor.screens.service';
 import { AlertComponent } from "../../../../../shared/components/alert/alert.component";
 import { NavbarComponent } from "../../../../../core/components/navbar/navbar.component";
@@ -11,33 +12,49 @@ import { AdvertisementSpaceComponent } from "../../../../../shared/components/ad
 @Component({
   selector: 'app-contact-persons',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, TableModule, AdvertisementSpaceComponent, AlertComponent],
+  imports: [CommonModule, NavbarComponent, TableModule, AdvertisementSpaceComponent, AlertComponent, NameInitialsPipe],
   templateUrl: './contact-persons.component.html',
   styleUrl: './contact-persons.component.scss'
 })
 
 export class ContactPersonsComponent {
   access$ =new Observable<any>();
+  revoke$ =new Observable<any>();
   contactPersons:ContactPerson[] =[];
   investorProfile: InvestorProfile | null = null;
   private _screenService = inject(InvestorScreensService);
 
   cols =[
-    { field: 'name', header: 'Name' },
-    { field: 'email', header: 'Email' },
-    { field: 'phone', header: 'Phone' },
-    { field: 'primaryContact', header: 'Primary Contact' },
+    { field: 'profile', header: 'Profile' },
+    { field: 'role', header: 'Role' },
     { field: 'action', header: 'Action' },
   ]
+  investorProfile$ =new Observable();
 
+  getContactPersons(){
+    this.investorProfile$ = this._screenService.getInvestorProfileById().pipe(tap(investorProfile => {
+      this.investorProfile = investorProfile;
+      this.contactPersons =[{id: investorProfile.investor.id, emailAddress: investorProfile.investor.username, designation: 'super admin', firstName: investorProfile.investor.firstName, hasAccess: true, phoneNumber: '', primaryContact: true, lastName: investorProfile.investor.lastName}, ...investorProfile.contactPersons];
+      this.contactPersons =this.contactPersons.map(contactPerson =>({...contactPerson, name: `${contactPerson.firstName} ${contactPerson.lastName}`.trim()}));
+    }))
+  }
 
-  investorProfile$ = this._screenService.getInvestorProfileById().pipe(tap(investorProfile => {
-    this.investorProfile = investorProfile;
-    this.contactPersons =investorProfile.contactPersons;
-  }))
+  ngOnInit() {
+    this.getContactPersons();
+  }
 
   grantAccess(contactPersonId:number){
     const investorProfileId =this.investorProfile?.id
-    this.access$ =this._screenService.grantAccess(investorProfileId, contactPersonId)
+    this.access$ =this._screenService.grantAccess(investorProfileId, contactPersonId).pipe(tap(res =>{
+      this.getContactPersons();
+    }))
   }
+
+  revokeAccess(contactPersonId:number){
+    const investorProfileId =this.investorProfile?.id;
+    this.revoke$ =this._screenService.revokeAccess(investorProfileId, contactPersonId).pipe(tap(res =>{
+      this.getContactPersons();
+    }))
+  }
+
 }
