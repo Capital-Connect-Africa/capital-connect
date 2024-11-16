e1a8f5f7994454c7c41cc934b3c5d2786acdf735
meltus-bwire <meltusbwire10@gmail.com>
2024-11-16 18:16:17 +0300

    chore: created a service to perform crud on rules

---
+++ src/app/features/admin/services/rule.service.ts
@@ -0,0 +1,38 @@
+import { map } from "rxjs";
+import { Injectable } from "@angular/core";
+import { BASE_URL, BaseHttpService } from "../../../core";
+import { Rule, RuleFormData } from "../../../shared/interfaces/rule.interface";
+
+@Injectable({providedIn: 'root'})
+export class RulesService extends BaseHttpService{
+
+    getRules(page:number =1, limit:number =10, all:boolean =false){
+        return this.read(`${BASE_URL}/vouchers/rules/list?page=${page}&limit=${limit}&all=${all}`).pipe(map(res =>{
+            return res as Rule[]
+        }))
+    }
+
+    getRuleById(id: number){
+        return this.readById(`${BASE_URL}/vouchers/rules/get-by-id`, id).pipe(map((res:any) =>{
+            return res as Rule;
+        }))
+    }
+
+    updateRule(rule:RuleFormData, id: number){
+        return this.update(`${BASE_URL}/vouchers/rules`, id, rule).pipe(map((res:any) =>{
+            return res as Rule;
+        }))
+    }
+
+    removeRule(id:number){
+        return this.delete(`${BASE_URL}/vouchers/rules/remove`, id).pipe(map((res:any) =>{
+            return 
+        }))
+    }
+
+    createRule(rule:RuleFormData){
+        return this.create(`${BASE_URL}/vouchers/rules`, rule).pipe(map((res:any) =>{
+            return res as Rule
+        }))
+    }
+}
