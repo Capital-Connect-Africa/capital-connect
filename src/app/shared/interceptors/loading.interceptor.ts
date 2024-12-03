82248799c79c65bd42ef73e7ce7e167a191331a0
meltus-bwire <meltusbwire10@gmail.com>
2024-12-03 16:11:46 +0300

    fix: fixed user signup blocker

--- src/app/shared/interceptors/loading.interceptor.ts
+++ src/app/shared/interceptors/loading.interceptor.ts
@@ -2,13 +2,16 @@ import { inject } from '@angular/core';
 import { HttpInterceptorFn } from '@angular/common/http';
 import { finalize } from 'rxjs/operators';
 import { LoadingService } from '../../core';
+import { SignalsService } from '../../core/services/signals/signals.service';
 
 export const LoadingInterceptor: HttpInterceptorFn = (req, next) => {
-  const _loadingService = inject(LoadingService)
+  const _loadingService = inject(LoadingService);
+  const _signalsService =inject(SignalsService);
   _loadingService.setLoading(true);
   return next(req).pipe(
     finalize(() => {
-      _loadingService.setLoading(false);
+      if(!_signalsService.fileUploading())
+        _loadingService.setLoading(false);
     })
   );
 }
