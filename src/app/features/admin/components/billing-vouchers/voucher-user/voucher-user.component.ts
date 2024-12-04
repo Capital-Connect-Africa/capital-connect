af4cd4c67d0bc4ba5f1f252c07a5a380b2fa96fb
meltus-bwire <meltusbwire10@gmail.com>
2024-12-04 23:20:29 +0300

    chore: added users

--- src/app/features/admin/components/billing-vouchers/voucher-user/voucher-user.component.ts
+++ src/app/features/admin/components/billing-vouchers/voucher-user/voucher-user.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { User } from '../../../../users/models';
+import { CommonModule } from '@angular/common';
+import { Voucher } from '../../../../../shared/interfaces/voucher.interface';
 
 @Component({
   selector: 'app-voucher-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './voucher-user.component.html',
   styleUrl: './voucher-user.component.scss'
 })
 export class VoucherUserComponent {
+  @Input() voucher!:Voucher;
+  users:User[] =this.voucher.users;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['voucher']) {
+      this.users = this.voucher.users;
+    }
+  }
 
 }
