6f85e1700acd67bd20a89aaa5c5f8f3fd7016b35
meltus-bwire <meltusbwire10@gmail.com>
2024-12-04 22:28:15 +0300

    chore: added a voucher details component

---
+++ src/app/features/admin/components/billing-vouchers/voucher-rules/voucher-rules.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voucher-rules',
+  standalone: true,
+  imports: [],
+  templateUrl: './voucher-rules.component.html',
+  styleUrl: './voucher-rules.component.scss'
+})
+export class VoucherRulesComponent {
+
+}
---
+++ src/app/features/admin/components/billing-vouchers/voucher-user/voucher-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voucher-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './voucher-user.component.html',
+  styleUrl: './voucher-user.component.scss'
+})
+export class VoucherUserComponent {
+
+}
