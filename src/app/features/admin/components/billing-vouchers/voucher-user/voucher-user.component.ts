1ec910e8209e725f0c5e552273476dd9a0998f57
meltus-bwire <meltusbwire10@gmail.com>
2024-12-10 16:31:45 +0300

    chore: fetch voucher details

--- src/app/features/admin/components/billing-vouchers/voucher-user/voucher-user.component.ts
+++ src/app/features/admin/components/billing-vouchers/voucher-user/voucher-user.component.ts
@@ -1,22 +1,23 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
 import { User } from '../../../../users/models';
 import { CommonModule } from '@angular/common';
-import { Voucher } from '../../../../../shared/interfaces/voucher.interface';
+import { Voucher, VoucherUser } from '../../../../../shared/interfaces/voucher.interface';
+import { TimeAgoPipe } from "../../../../../core/pipes/time-ago.pipe";
 
 @Component({
   selector: 'app-voucher-user',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TimeAgoPipe],
   templateUrl: './voucher-user.component.html',
   styleUrl: './voucher-user.component.scss'
 })
 export class VoucherUserComponent {
   @Input() voucher!:Voucher;
-  users:User[] =this.voucher.users;
+  voucherUsers:VoucherUser[] =[];
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['voucher']) {
-      this.users = this.voucher.users;
+    if (changes['voucher'] && this.voucher?.users) {
+      this.voucherUsers = this.voucher.users;
     }
   }
 
