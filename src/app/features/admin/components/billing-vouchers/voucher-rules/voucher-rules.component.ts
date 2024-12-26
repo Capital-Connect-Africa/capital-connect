6aef7cfc1e6b762a62fba0b29e765014b09926ca
meltus-bwire <meltusbwire10@gmail.com>
2024-12-26 09:17:43 +0300

    chore: added a voucher rules html component

--- src/app/features/admin/components/billing-vouchers/voucher-rules/voucher-rules.component.ts
+++ src/app/features/admin/components/billing-vouchers/voucher-rules/voucher-rules.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { Voucher, VoucherRule, VoucherUser } from '../../../../../shared/interfaces/voucher.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-voucher-rules',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './voucher-rules.component.html',
   styleUrl: './voucher-rules.component.scss'
 })
 export class VoucherRulesComponent {
-
+  @Input() voucher!:Voucher;
+  voucherRules:VoucherRule[] =[];
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['voucher'] && this.voucher?.users) {
+      this.voucherRules = this.voucher.rules;
+    }
+  }
 }
