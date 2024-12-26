42e483e2774f6c63bfefc459762de82354921ff1
meltus-bwire <meltusbwire10@gmail.com>
2024-12-26 09:24:10 +0300

    chore: modified the voucher rules user properties type

--- src/app/shared/interfaces/voucher.interface.ts
+++ src/app/shared/interfaces/voucher.interface.ts
@@ -6,6 +6,24 @@ export enum VoucherType{
     subscriptionPlan ='subscription-plan',
 }
 
+export enum UserProperties{
+    referredBy,
+    createdAt,
+    isEmailVerified,
+    hasAcceptedTerms,
+    roles,
+}
+
+export enum Operators{
+    EQUAL_TO ='eq',
+    LESS_THAN ='lt',
+    GREATER_THAN ='gt',
+    LESS_THAN_OR_EQUAL_TO ='lte',
+    GREATER_THAN_OR_EQUAL_TO ='gte',
+    BETWEEN ='between',
+    EXISTS ='exists,'
+}
+
 export interface VoucherBase{
     type: VoucherType
     maxUses: number
