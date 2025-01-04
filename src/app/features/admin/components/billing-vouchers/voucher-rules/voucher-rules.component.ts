4315ccd40bea72ad3abd69416b00cd3cdd2adeec
meltus-bwire <meltusbwire10@gmail.com>
2025-01-04 07:18:28 +0300

    chore: setup the rules ui modal

--- src/app/features/admin/components/billing-vouchers/voucher-rules/voucher-rules.component.ts
+++ src/app/features/admin/components/billing-vouchers/voucher-rules/voucher-rules.component.ts
@@ -1,22 +1,49 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, inject, Input, SimpleChanges } from '@angular/core';
 import { Rule } from '../../../../../shared/interfaces/rule.interface';
 import { Voucher, VoucherType } from '../../../../../shared/interfaces/voucher.interface';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ModalComponent } from "../../../../../shared/components/modal/modal.component";
 
 @Component({
   selector: 'app-voucher-rules',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MultiSelectModule, CalendarModule, DropdownModule, ModalComponent, ReactiveFormsModule],
   templateUrl: './voucher-rules.component.html',
   styleUrl: './voucher-rules.component.scss'
 })
+
 export class VoucherRulesComponent {
+
   @Input() voucher!:Voucher;
   voucherRules:Rule[] =[];
-  VoucherPurchase =VoucherType
+  VoucherPurchase =VoucherType;
+  private _fb =inject(FormBuilder)
+  helperText ="";
+  heading ="New Constraint";
+  visible =true;
+
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['voucher'] && this.voucher?.users) {
       this.voucherRules = this.voucher.rules;
     }
+    this.helperText =`Add a contraint to the voucher ${this.voucher.code}`
+  }
+
+  ruleForm =this._fb.group({
+    userPropery: ['', [Validators.required]],
+    description: ['', [Validators.required]],
+    operator: ['', [Validators.required]],
+    value: ['', [Validators.required]],
+  })
+
+  saveRule() {
+  }
+
+  showModal(){
+    this.visible =true;
   }
 }
