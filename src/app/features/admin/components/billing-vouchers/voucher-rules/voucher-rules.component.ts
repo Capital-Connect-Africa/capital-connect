4c367977ee1fcf033844d4099a84cdcaedfe6896
meltus-bwire <meltusbwire10@gmail.com>
2025-01-07 09:43:11 +0300

    Chore: added a dynamic rules form

--- src/app/features/admin/components/billing-vouchers/voucher-rules/voucher-rules.component.ts
+++ src/app/features/admin/components/billing-vouchers/voucher-rules/voucher-rules.component.ts
@@ -1,17 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input, SimpleChanges } from '@angular/core';
 import { Rule } from '../../../../../shared/interfaces/rule.interface';
-import { Voucher, VoucherType } from '../../../../../shared/interfaces/voucher.interface';
+import { OperatorOption, Operators, UserProperties, Voucher, VoucherFormRuleFields, VoucherType } from '../../../../../shared/interfaces/voucher.interface';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
 import { DropdownModule } from 'primeng/dropdown';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { ModalComponent } from "../../../../../shared/components/modal/modal.component";
+import { TimeAgoPipe } from "../../../../../core/pipes/time-ago.pipe";
+import { Option } from '../../../../../shared/interfaces/options.interface';
 
 @Component({
   selector: 'app-voucher-rules',
   standalone: true,
-  imports: [CommonModule, MultiSelectModule, CalendarModule, DropdownModule, ModalComponent, ReactiveFormsModule],
+  imports: [CommonModule, MultiSelectModule, CalendarModule, DropdownModule, ModalComponent, ReactiveFormsModule, TimeAgoPipe],
   templateUrl: './voucher-rules.component.html',
   styleUrl: './voucher-rules.component.scss'
 })
@@ -20,6 +22,9 @@ export class VoucherRulesComponent {
 
   @Input() voucher!:Voucher;
   voucherRules:Rule[] =[];
+  operators:OperatorOption[] =[];
+  operator:Operators | null =null;
+  options:Option[] =[]
   VoucherPurchase =VoucherType;
   private _fb =inject(FormBuilder)
   helperText ="";
@@ -41,9 +46,46 @@ export class VoucherRulesComponent {
   })
 
   saveRule() {
+
   }
 
   showModal(){
     this.visible =true;
+  }
+
+  formFields:VoucherFormRuleFields[] =[
+    {
+      userPropertyName: 'Role',
+      userPropertyValue: UserProperties.roles,
+      operator: [{value: Operators.EQUAL_TO, label: 'User Is'}],
+      valueType: 'select',
+      options: [{value: 'user', label: 'Business'}, {value: 'investor', label: 'Investor'}]
+    }
+  ]
+
+  handleUserPropertyChange(event:any){
+    const { value } =event;
+    if(value){
+      const operators =this.formFields.find(field =>field.userPropertyValue ==value);
+      if(operators){
+        this.operators =operators.operator
+        this.options =operators.options
+      }else{
+        this.options =[];
+        this.operators =[]
+      }
+    }else{
+      this.operators =[];
+    }
+  }
+
+  handleOperatorChange(event: any){
+    const { value } =event
+    
+    if(value){
+      this.operator =value;
+    }else{
+      this.operator =null;
+    } 
   }
 }
