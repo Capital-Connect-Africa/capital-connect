974816cc360ce83a2ce87087a4a3a476530848cb
meltus-bwire <meltusbwire10@gmail.com>
2025-01-10 07:46:08 +0300

    chore: implement rules creation logic

--- src/app/features/admin/components/billing-vouchers/voucher-rules/voucher-rules.component.ts
+++ src/app/features/admin/components/billing-vouchers/voucher-rules/voucher-rules.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input, SimpleChanges } from '@angular/core';
-import { Rule } from '../../../../../shared/interfaces/rule.interface';
+import { Rule, RuleFormData } from '../../../../../shared/interfaces/rule.interface';
 import { OperatorOption, Operators, UserProperties, Voucher, VoucherFormRuleFields, VoucherType } from '../../../../../shared/interfaces/voucher.interface';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
@@ -9,6 +9,9 @@ import { MultiSelectModule } from 'primeng/multiselect';
 import { ModalComponent } from "../../../../../shared/components/modal/modal.component";
 import { TimeAgoPipe } from "../../../../../core/pipes/time-ago.pipe";
 import { Option } from '../../../../../shared/interfaces/options.interface';
+import { Observable, switchMap, tap } from 'rxjs';
+import { RulesService } from '../../../services/rule.service';
+import { BillingVoucherService } from '../../../services/billing-voucher.service';
 
 @Component({
   selector: 'app-voucher-rules',
@@ -24,12 +27,16 @@ export class VoucherRulesComponent {
   voucherRules:Rule[] =[];
   operators:OperatorOption[] =[];
   operator:Operators | null =null;
-  options:Option[] =[]
+  options:Option[] =[];
   VoucherPurchase =VoucherType;
-  private _fb =inject(FormBuilder)
+  private _fb =inject(FormBuilder);
   helperText ="";
   heading ="New Constraint";
   visible =true;
+  createRule$ =new Observable();
+  updateVoucherRule$ =new Observable();
+  private _rulesService =inject(RulesService);
+  private _voucherService =inject(BillingVoucherService);
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['voucher'] && this.voucher?.users) {
@@ -39,15 +46,23 @@ export class VoucherRulesComponent {
   }
 
   ruleForm =this._fb.group({
-    userPropery: ['', [Validators.required]],
+    userProperty: ['', [Validators.required]],
     description: ['', [Validators.required]],
     operator: ['', [Validators.required]],
     value: ['', [Validators.required]],
   })
 
-  saveRule() {
-
-  }
+  saveRule(){
+    const values =this.ruleForm.value as Partial<RuleFormData>;
+    this.createRule$ =this._rulesService.createRule(values).pipe(switchMap(rule =>{
+      return this._voucherService.updateBillingVoucher({rules: [rule.id]}, this.voucher.id).pipe(tap(res =>{
+        this.voucher =res;
+        this.voucherRules =res.rules;
+        this.ruleForm.reset();
+        this.visible =false;
+      }))
+    }));
+    }
 
   showModal(){
     this.visible =true;
@@ -56,7 +71,7 @@ export class VoucherRulesComponent {
   formFields:VoucherFormRuleFields[] =[
     {
       userPropertyName: 'Role',
-      userPropertyValue: UserProperties.roles,
+      userPropertyValue: UserProperties.ROLES,
       operator: [{value: Operators.EQUAL_TO, label: 'User Is'}],
       valueType: 'select',
       options: [{value: 'user', label: 'Business'}, {value: 'investor', label: 'Investor'}]
