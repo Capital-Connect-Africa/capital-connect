e45151dc8910ac3f270fb2f449178eea2db70143
=meltus-bwire <=meltusbwire10@gmail.com>
2025-01-22 02:57:58 +0300

    refactor: created a reusable copy link component

--- src/app/features/admin/pages/referals/referals.component.ts
+++ src/app/features/admin/pages/referals/referals.component.ts
@@ -2,27 +2,21 @@ import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { AdminUiContainerComponent } from "../../components/admin-ui-container/admin-ui-container.component";
 import { PieChartComponent } from "../../../../shared/components/charts/pie-chart/pie-chart.component";
 import { CommonModule } from '@angular/common';
-import { TimeAgoPipe } from "../../../../core/pipes/time-ago.pipe";
 import { TableModule } from 'primeng/table';
 import { Observable } from 'rxjs';
 import { ReferralsService } from '../../services/referrals.service';
-import { AuthStateService } from '../../../auth/services/auth-state.service';
+import { ReferralLinkComponent } from "../../../../shared/components/referral-link/referral-link.component";
 
 @Component({
   selector: 'app-referals',
   standalone: true,
-  imports: [AdminUiContainerComponent, PieChartComponent, CommonModule, TableModule],
+  imports: [AdminUiContainerComponent, PieChartComponent, CommonModule, TableModule, ReferralLinkComponent],
   templateUrl: './referals.component.html',
   styleUrl: './referals.component.scss'
 })
 export class ReferalsComponent {
   @ViewChild('textDiv') textDiv!: ElementRef<HTMLDivElement>;
   private _referralsService =inject(ReferralsService);
-  private _authStateService =inject(AuthStateService);
-  link ='https://app.capitalconnect.africa/signup?referralId='
-  token =this._authStateService.currentUserProfile().referralToken;
-
-  linkCopied =false;
   cols =[
     {header: 'RNK', field: 'rnk'},
     {header: 'User', field: 'user'},
@@ -58,23 +52,6 @@ export class ReferalsComponent {
 
   ngOnInit(){
     this.getLeadersBoard();
-  }
-
-  async copyToClipboard(): Promise<void> {
-    const range = document.createRange();
-    const selection = window.getSelection();
-  
-    if (this.textDiv.nativeElement && selection) {
-      range.selectNodeContents(this.textDiv.nativeElement);
-      selection.removeAllRanges();
-      selection.addRange(range);
-      try {
-        await navigator.clipboard.writeText(`${this.link}${this.token}`)
-        this.linkCopied = true;
-      } catch (error) {
-        this.linkCopied = false;
-      }
-    }
   }
 
   getLeadersBoard(page =1, limit =10){
