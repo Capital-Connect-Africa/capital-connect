87b980883c5e70a64d4a714806455857b301570c
=meltus-bwire <=meltusbwire10@gmail.com>
2025-01-22 11:27:58 +0300

    chore: added a method to user referral metrics

--- src/app/features/admin/services/referrals.service.ts
+++ src/app/features/admin/services/referrals.service.ts
@@ -1,23 +1,41 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { BASE_URL, BaseHttpService } from "../../../core";
-import { map } from "rxjs";
+import { firstValueFrom, map } from "rxjs";
+import { ReferralTokenService } from "../../../shared/services/referral-token.service";
+import { AuthStateService } from "../../auth/services/auth-state.service";
 
 @Injectable({providedIn: 'root'})
 
 export class ReferralsService extends BaseHttpService{
+    private _authStateService =inject(AuthStateService);
+    private _referralTokenService =inject(ReferralTokenService);
 
     getLeadersBoard(page:number, limit:number){
-        return this.read(`${BASE_URL}/referrals?page=${page}&limit=${limit}`).pipe(map((referrals:any) =>{
-            debugger
+        return this.read(`${BASE_URL}/referrals?page=${page}&limit=${limit}`).pipe(map((referrals:any[]) =>{
+            return referrals.map(referral =>{{
+                
+            }})
         }))
     }
 
-    clickReferralLink(){
-        return 
-    }
-
-    visitReferralLink(){
-        return 
+    async updateMetrics(page:string, visits:boolean, clicks:boolean, referralId:string | null =null): Promise<any>{
+        if(this._authStateService.isLoggedIn) return;
+        if(referralId) this._referralTokenService.saveReferralToken(referralId);
+        const referral =this._referralTokenService.getReferralToken();
+        if(referral){
+            const token =referral.token;
+            const pages =referral.pages || []
+            if(!token) return
+            let visiting =visits && !pages.includes(page);
+            if(visiting) {
+                pages.push(page)
+                referral.pages =pages;
+                localStorage.setItem('referral', JSON.stringify(referral));
+            }
+            if(clicks || visiting) {
+                return await firstValueFrom(this.update(`${BASE_URL}/referrals/metrics`, token, {visits: visiting, clicks}));
+            }
+        }
     }
 
 }
