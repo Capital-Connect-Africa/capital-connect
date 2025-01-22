d52b9ce9237a2ea07ce2e0ef5aacc54a64e2bd82
=meltus-bwire <=meltusbwire10@gmail.com>
2025-01-22 11:49:39 +0300

    chore: fetch stats from the backend

--- src/app/features/admin/services/referrals.service.ts
+++ src/app/features/admin/services/referrals.service.ts
@@ -1,8 +1,9 @@
 import { inject, Injectable } from "@angular/core";
 import { BASE_URL, BaseHttpService } from "../../../core";
-import { firstValueFrom, map } from "rxjs";
+import { firstValueFrom, map, Observable } from "rxjs";
 import { ReferralTokenService } from "../../../shared/services/referral-token.service";
 import { AuthStateService } from "../../auth/services/auth-state.service";
+import { ReferralLeader } from "../interfaces/referral.leader.interface";
 
 @Injectable({providedIn: 'root'})
 
@@ -10,11 +11,15 @@ export class ReferralsService extends BaseHttpService{
     private _authStateService =inject(AuthStateService);
     private _referralTokenService =inject(ReferralTokenService);
 
-    getLeadersBoard(page:number, limit:number){
+    getLeadersBoard(page:number, limit:number): Observable<ReferralLeader[]>{
         return this.read(`${BASE_URL}/referrals?page=${page}&limit=${limit}`).pipe(map((referrals:any[]) =>{
-            return referrals.map(referral =>{{
-                
-            }})
+            return referrals.map(referral =>({
+                name: `${referral.user.firstName} ${referral.user.lastName}`.trim(),
+                clicks: referral.clicks,
+                visits: referral.visits,
+                signups: referral.user.referredUsers.length,
+                rate: `${Math.round((referral.user.referredUsers.length/referral.clicks) *100)}%`
+            })).sort((a, b) =>a.signups - b.signups).map((referral, index) =>({rank: index+1, ...referral}))
         }))
     }
 
