9a8f5524bc16555b8a6a3b317ee59fca996a7b59
=meltus-bwire <=meltusbwire10@gmail.com>
2025-01-22 11:25:41 +0300

    chore: added capability to save a referral token

--- src/app/shared/services/referral-token.service.ts
+++ src/app/shared/services/referral-token.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from "@angular/core";
 
 @Injectable({providedIn: 'root'})
 
-export class ReferralTokenService{
-    getReferralToken(): string | null{
-        const { exp, token } =JSON.parse(localStorage.getItem('referral') || '{}') as {exp: Date, token: string}
+export class ReferralTokenService {
+
+    getReferralToken(){
+        const { exp, token, pages } =JSON.parse(localStorage.getItem('referral') || '{}') as {exp: Date, token: string, pages: string[]}
         if(token && exp){
-            if(exp.getTime() >new Date().getTime()){
-                return token;
+            if(new Date(exp).getTime() >new Date().getTime()){
+                return { token, pages}
             }else this.removeToken()
         }
         return null;
@@ -15,5 +16,20 @@ export class ReferralTokenService{
 
     removeToken(){
         localStorage.removeItem('referral')
+    }
+
+    saveReferralToken(referralId:string | null){
+        if(!referralId) return;
+        const now =new Date();
+        const then =new Date();
+        then.setDate(now.getDate() +90);
+        const referral =JSON.parse(localStorage.getItem('referral') as string)
+        if(!referral){
+            localStorage.setItem('referral', JSON.stringify({
+                token: referralId,
+                exp: then,
+                pages: []
+            }))
+        }
     }
 }
