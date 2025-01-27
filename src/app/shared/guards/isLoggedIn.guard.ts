2b2e8530534a2061374016ba5d0499a6ff2c2719
=meltus-bwire <=meltusbwire10@gmail.com>
2025-01-27 14:31:55 +0300

    chore: pick r from the query params

--- src/app/shared/guards/isLoggedIn.guard.ts
+++ src/app/shared/guards/isLoggedIn.guard.ts
@@ -1,5 +1,11 @@
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateChildFn, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateChildFn,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { AuthStateService } from '../../features/auth/services/auth-state.service';
 import { FORM_TYPE } from '../../features/auth/interfaces/auth.interface';
 import { ReferralsService } from '../../features/admin/services/referrals.service';
@@ -9,30 +15,32 @@ export const isLoggedInCanActivateGuard: CanActivateFn = (
   state: RouterStateSnapshot
 ) => {
   return checkLogin(route, state);
-}
+};
 
 export const isLoggedInCanActivateChildGuard: CanActivateChildFn = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
   return checkLogin(route, state);
-}
+};
 
-async function checkLogin(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+async function checkLogin(
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) {
   const authStateService = inject(AuthStateService);
   const router = inject(Router);
-  const referralService =inject(ReferralsService);
-
+  const referralService = inject(ReferralsService);
   // Check if the URL is empty
   const url = state.url;
-  const { referralId } =route.queryParams;
+  const { r } = route.queryParams; // r: stands referral id
   if (url === '/') {
-     window.history.pushState(null, '', '/');
+    window.history.pushState(null, '', '/');
     if (authStateService.isLoggedIn && !authStateService.userIsAdmin) {
       router.navigateByUrl('/user-profile');
       return false;
-    }  else if(authStateService.userIsAdmin){
-      router.navigateByUrl('/dashboard')
+    } else if (authStateService.userIsAdmin) {
+      router.navigateByUrl('/dashboard');
       return false;
     }
     return true;
@@ -41,13 +49,15 @@ async function checkLogin(route: ActivatedRouteSnapshot, state: RouterStateSnaps
   if (authStateService.isLoggedIn) {
     return true;
   }
-  let signup =false;  
-    
-  if(referralId){
-    signup =true;
-    await referralService.updateMetrics('', false, true, referralId);
+  let signup = false;
+
+  if (r) {
+    signup = true;
+    await referralService.updateMetrics('', false, true, r);
   }
-  
-  router.navigateByUrl('/', { state: { mode: signup? FORM_TYPE.SIGNUP : FORM_TYPE.SIGNIN  } });
+
+  router.navigateByUrl('/', {
+    state: { mode: signup ? FORM_TYPE.SIGNUP : FORM_TYPE.SIGNIN },
+  });
   return false;
 }
