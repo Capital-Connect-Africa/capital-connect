9fc395a341150186b62669fdc89829cf0baafff1
=meltus-bwire <=meltusbwire10@gmail.com>
2025-01-27 13:20:21 +0300

    refactor: use a shorter link instead

--- src/app/shared/components/referral-link/referral-link.component.ts
+++ src/app/shared/components/referral-link/referral-link.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, inject, ViewChild } from '@angular/core';
-import { AppQrCodeComponent } from "../app-qr-code/app-qr-code.component";
+import { AppQrCodeComponent } from '../app-qr-code/app-qr-code.component';
 import { AuthStateService } from '../../../features/auth/services/auth-state.service';
 
 @Component({
@@ -8,25 +8,26 @@ import { AuthStateService } from '../../../features/auth/services/auth-state.ser
   standalone: true,
   imports: [CommonModule, AppQrCodeComponent],
   templateUrl: './referral-link.component.html',
-  styleUrl: './referral-link.component.scss'
+  styleUrl: './referral-link.component.scss',
 })
 export class ReferralLinkComponent {
   @ViewChild('textDiv') textDiv!: ElementRef<HTMLDivElement>;
-  private _authStateService =inject(AuthStateService);
-    link ='https://app.capitalconnect.africa/signup?referralId='
-    token =this._authStateService.currentUserProfile().referralToken;
-  
-    linkCopied =false;
+  private _authStateService = inject(AuthStateService);
+  link = `https://app.capitalconnect.africa/r/${
+    this._authStateService.currentUserProfile().referralCode
+  }`;
+
+  linkCopied = false;
   async copyToClipboard(): Promise<void> {
     const range = document.createRange();
     const selection = window.getSelection();
-  
+
     if (this.textDiv.nativeElement && selection) {
       range.selectNodeContents(this.textDiv.nativeElement);
       selection.removeAllRanges();
       selection.addRange(range);
       try {
-        await navigator.clipboard.writeText(`${this.link}${this.token}`)
+        await navigator.clipboard.writeText(this.link);
         this.linkCopied = true;
       } catch (error) {
         this.linkCopied = false;
