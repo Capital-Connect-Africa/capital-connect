346f5172a554de6fd2f5ae143a7fafc0ce2b37fb
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-05 17:28:21 +0300

    feat: added a single business user details page

---
+++ src/app/features/partner/pages/business-user/business-user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { PartnerLayoutComponent } from "../../components/layout/layout.component";
+
+@Component({
+  selector: 'app-business-user',
+  standalone: true,
+  imports: [PartnerLayoutComponent],
+  templateUrl: './business-user.component.html',
+  styleUrl: './business-user.component.scss'
+})
+export class BusinessUserComponent {
+
+}
