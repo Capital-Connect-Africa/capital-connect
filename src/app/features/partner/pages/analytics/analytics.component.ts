4d629e31eb9d86c2dc6b9405eb16eb45c61f3d2b
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-07 01:29:34 +0300

    feat: created the analytics feature

---
+++ src/app/features/partner/pages/analytics/analytics.component.ts
@@ -0,0 +1,41 @@
+import { Component, inject } from '@angular/core';
+import { PartnerLayoutComponent } from '../../components/layout/layout.component';
+import { UserStatisticsService } from '../../../admin/services/user.statistics.service';
+import { tap } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { ColumnChartComponent } from "../../../../shared/components/charts/column-chart/column-chart.component";
+import { HorizontalBarchartComponent } from "../../../../shared/components/charts/horizontal-barchart/horizontal-barchart.component";
+import { PieChartComponent } from "../../../../shared/components/charts/pie-chart/pie-chart.component";
+import { GeoChartComponent } from "../../../../shared/components/charts/geo-chart/geo-chart.component";
+
+@Component({
+  selector: 'app-analytics',
+  standalone: true,
+  imports: [PartnerLayoutComponent, CommonModule, ColumnChartComponent, HorizontalBarchartComponent, PieChartComponent, GeoChartComponent],
+  templateUrl: './analytics.component.html',
+  styleUrl: './analytics.component.scss'
+})
+export class AnalyticsComponent {
+  private _statsService =inject(UserStatisticsService)
+  businessStages:Record<string, number> ={};
+  businessSectors:Record<string, number> ={};
+  businessFundRaise:Record<string, number> ={};
+  businessMaxFunding:Record<string, number> ={};
+  businessMinFunding:Record<string, number> ={};
+  businessCountries:Record<string, number> ={};
+  investorSectors:Record<string, number> ={};
+  businessFundingType:Record<string, number> ={};
+  investorFundingType:Record<string, number> ={};
+  analytics$ =this._statsService.getAnalytics().pipe(tap(analytics =>{
+    this.businessStages =analytics.stages;
+    this.businessSectors =analytics.sectors.companies;
+    this.investorSectors =analytics.sectors.investors;
+    this.businessFundRaise =analytics.fund_raise;
+    this.businessMaxFunding =analytics.max_funding;
+    this.businessMinFunding =analytics.min_funding
+    this.businessCountries =analytics.countries
+    this.businessFundingType =analytics.funding.companies
+    this.investorFundingType =analytics.funding.investors
+    debugger
+  }))
+}
