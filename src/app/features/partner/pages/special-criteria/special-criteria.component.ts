a4d40da8f27a406f47bd4560483e6e1bfd09f6c1
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-07 13:33:40 +0300

    feat: created the special criteria feature for te partner profile

---
+++ src/app/features/partner/pages/special-criteria/special-criteria.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { PartnerLayoutComponent } from "../../components/layout/layout.component";
+
+@Component({
+  selector: 'app-special-criteria',
+  standalone: true,
+  imports: [PartnerLayoutComponent],
+  templateUrl: './special-criteria.component.html',
+  styleUrl: './special-criteria.component.scss'
+})
+export class SpecialCriteriaComponent {
+
+}
