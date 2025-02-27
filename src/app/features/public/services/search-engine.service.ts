54e3afef6a21ae2dd89b63753525c51acac3bf0d
=meltus-bwire <=meltusbwire10@gmail.com>
2025-02-27 19:11:12 +0300

    chore: added a service to handle crud

---
+++ src/app/features/public/services/search-engine.service.ts
@@ -0,0 +1,38 @@
+import { inject, Injectable } from '@angular/core';
+import { CountriesService } from '../../../shared/services/countries.service';
+import { InvestorScreensService } from '../../investor/services/investor.screens.service';
+import { SectorsService } from '../../sectors/services/sectors/sectors.service';
+import { BehaviorSubject, combineLatest, map } from 'rxjs';
+import { Country } from '../../../shared/interfaces/countries';
+import { Sector } from '../../sectors/interfaces';
+import { UseOfFundsOptions } from '../../../shared/interfaces/Investor';
+import { PublicInvestor, UserSearch } from '../../../shared/interfaces/public.investor.interface';
+import { PublicInvestorsRepositoryService } from '../../../core/services/investors/public-investors-repository.service';
+
+@Injectable({ providedIn: 'root' })
+export class SearchEngineService {
+
+    results$ =new BehaviorSubject<{q?: string, data?: PublicInvestor[]}>({})
+
+    private _sectorsService = inject(SectorsService);
+    private _countriesService = inject(CountriesService);
+    private _investorsService = inject(InvestorScreensService);
+    private _publicInvestorService = inject(PublicInvestorsRepositoryService);
+
+    fetchOptions(){
+        const requests =[this._sectorsService.getAllSectors(), this._countriesService.getCountries(), this._investorsService.getUseOfFunds()]
+        return combineLatest(requests).pipe(map(res =>{
+            const [sectors, countries, useOfFunds] =res
+            return {
+                sectors: sectors as Sector[],
+                useOfFunds: (useOfFunds as UseOfFundsOptions[]).map(useOfFund =>useOfFund.title),
+                countries: (countries as Country[]).map(country =>country.name)
+            }
+        }))
+    }
+
+    searchInvestors(payload: Partial<UserSearch>){
+        return this._publicInvestorService.searchInvestors(payload)
+    }
+
+}
