5e69431e1a5c552d5e194010459c0fc57d0a9c05
meltus <meltusbwire10@gmail.com>
2025-03-07 12:31:50 +0300

    chore: removed the referrers to the profile page

--- src/app/features/staff/pages/dashboard/dashboard.component.ts
+++ src/app/features/staff/pages/dashboard/dashboard.component.ts
@@ -1,26 +1,19 @@
 import { Component, inject } from '@angular/core';
-import { ReferralLinkComponent } from "../../../../shared/components/referral-link/referral-link.component";
-import { ReferralsService } from '../../../admin/services/referrals.service';
-import { AuthStateService } from '../../../auth/services/auth-state.service';
-import { tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { User } from '../../../users/models';
+import { SignalsService } from '../../../../core/services/signals/signals.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ReferralLinkComponent, CommonModule],
+  imports: [ CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  private _signalService =inject(SignalsService);
 
-  myReferres:User[] =[]
-  private _authStateService =inject(AuthStateService);
-  private _referralsService =inject(ReferralsService);
-
-  myReferrees$ =this._referralsService.getMyReferees(this._authStateService.currentUserId()).pipe(tap(res =>{
-    this.myReferres =res;
-  }))
+  ngOnInit(): void {
+    this._signalService.pageTitle.set('Dashboard');
+  }
 
 }
