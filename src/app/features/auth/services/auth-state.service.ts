e320b8859d202f205128bb0a5e0e88528e9938d6
=meltus <=meltusbwire10@gmail.com>
2025-03-09 18:15:14 +0300

    chore: added logic to optionally redirect user on logout

--- src/app/features/auth/services/auth-state.service.ts
+++ src/app/features/auth/services/auth-state.service.ts
@@ -112,28 +112,29 @@ export class AuthStateService {
     this.currentUserName.set(userName);
   }
 
-  private clearSession(feedback: boolean =true){
+  private clearSession(feedback: boolean =true, redirectUser:boolean =true){
     this.reset();
     if(feedback) this._feedBackService.success('Logged Out! See you soon!');
-    this._router.navigateByUrl('/', {
-      state: { mode: FORM_TYPE.SIGNIN },
-    });
+    if(redirectUser)
+      this._router.navigateByUrl('/', {
+        state: { mode: FORM_TYPE.SIGNIN },
+      });
   }
 
-  logout(silent =false) {
+  logout(silent =false, redirectUser:boolean =true) {
     if(!silent){
       return this._confirmationService
         .confirm('Are you sure you want to log out?')
         .pipe(
           tap((confirmation) => {
             if (confirmation) {
-              this.clearSession()
+              this.clearSession(redirectUser)
             }
           })
         );
     }
-    this.clearSession(false)
-    return of() as any
+    this.clearSession(false, redirectUser)
+    return of() as any;
   }
 
   saveUserPhoneNumberAddedStatus(phoneNo: string) {
