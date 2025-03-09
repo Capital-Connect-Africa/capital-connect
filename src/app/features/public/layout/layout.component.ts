ad5a853e0281a1b1fe2893ae5b5137fa4d31b34f
=meltus <=meltusbwire10@gmail.com>
2025-03-09 18:15:50 +0300

    chore: added a logout button

--- src/app/features/public/layout/layout.component.ts
+++ src/app/features/public/layout/layout.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { AuthStateService } from '../../auth/services/auth-state.service';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -10,9 +12,19 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './layout.component.scss'
 })
 export class PublicLayoutComponent {
+  logout$ =new Observable()
+  private _authStateService =inject(AuthStateService)
+  isLoggedIn:boolean =this._authStateService.isLoggedIn;
   partners =[
     {logo: 'assets/img/MEDA.webp', url: 'https://www.meda.org/', name: 'MEDA', height: 'h-6'},
     {logo: 'assets/img/KCIC.png', url: 'https://www.kenyacic.org/', name: 'KCIC', height: 'h-9'},
     {logo: 'assets/img/Raisin.png', url: 'https://raisin.co.ke/', name: 'Raisin', height: 'h-9'},
   ]
+
+  logOut(){
+    this.logout$ =this._authStateService.logout(false, false).pipe(tap(() =>{
+      this.isLoggedIn =false
+
+    }))
+  }
 }
