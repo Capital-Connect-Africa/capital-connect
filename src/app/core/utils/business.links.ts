56a994327c5ae91c6bb938f7c92a9474e0809921
=meltus-bwire <=meltusbwire10@gmail.com>
2025-03-10 13:57:46 +0300

    wip: commented out the investors link

--- src/app/core/utils/business.links.ts
+++ src/app/core/utils/business.links.ts
@@ -23,11 +23,11 @@ export class BusinessLinkService {
 
 export const BusinessLinks = [
     { label: 'Dashboard', href: '/business', exact: true, icon: 'grid_view', display: true },
+    // { label: 'Investors', href: '/business/investors-db', exact: true, icon: 'storage', display: true },
     { label: 'Plans', href: '/business/plans', exact: false, icon: 'paid', display: false},
     { label: 'My Business', href: '/business/my-business', exact: false, icon: 'business_center', display: true },
     { label: 'Special Criteria', href: '/business/special-criteria', exact: false, icon: 'contact_support', display: true },
     { label: 'My Bookings', href: '/business/my-bookings', exact: false, icon: 'event', display: true },
-    { label: 'Investors', href: '/business/investors-db', exact: true, icon: 'storage', display: true },
     { label: 'Financials', href: '', exact: true,   icon: 'bar_chart',  display: true,
       children: [
         { label: 'Income Statement', href: '/business/financial-reporting', exact: true, icon: 'show_chart' }, // Updated icon
