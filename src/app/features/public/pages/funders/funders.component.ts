94c316350321012b6c798524fe47e3d29991b270
=meltus-bwire <=meltusbwire10@gmail.com>
2025-03-10 13:41:27 +0300

    chore: added a search results page

--- src/app/features/public/pages/funders/funders.component.ts
+++ src/app/features/public/pages/funders/funders.component.ts
@@ -1,39 +1,29 @@
 import { Component, inject } from '@angular/core';
-import {
-  AllCommunityModule,
-  ModuleRegistry,
-  themeMaterial,
-  ColDef,
-  GridOptions,
-  ValueFormatterParams,
-  iconSetMaterial,
-  GridApi,
-} from 'ag-grid-community';
 import { AgGridAngular } from 'ag-grid-angular';
 import { PublicInvestor } from '../../../../shared/interfaces/public.investor.interface';
-import { formatCurrency } from '../../../../core/utils/format.currency';
 import { Observable, tap } from 'rxjs';
 import { PublicInvestorsRepositoryService } from '../../../../core/services/investors/public-investors-repository.service';
 import { SearchEngineService } from '../../services/search-engine.service';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
-
-ModuleRegistry.registerModules([AllCommunityModule]);
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthStateService } from '../../../auth/services/auth-state.service';
 
 @Component({
   selector: 'app-funders',
   standalone: true,
-  imports: [AgGridAngular, CommonModule],
+  imports: [CommonModule],
   templateUrl: './funders.component.html',
   styleUrl: './funders.component.scss'
 })
 export class FundersComponent {
+  private _route =inject(Router);
   private _activatedRoute =inject(ActivatedRoute);
+  private _authStateService =inject(AuthStateService);
   private _searchEngineService =inject(SearchEngineService);
   private _publicInvestorService = inject(PublicInvestorsRepositoryService);
 
   
-  q =this._activatedRoute.snapshot.params['q']
+  q =this._activatedRoute.snapshot.params['search-key']
   searchedResults$ =this._searchEngineService.results$.pipe(tap(res =>{
     if(res.investors){
       this.publicInvestors =res.investors
@@ -51,57 +41,12 @@ export class FundersComponent {
       this.q =res.q
     }))
   }
-    
 
-
-
-  gridApi!: GridApi<Partial<PublicInvestor>>;
-    selectedColumns: string[] = [];
-    theme = themeMaterial
-      .withPart(iconSetMaterial)
-      .withParams({
-        iconSize: 18,
-        wrapperBorderRadius: '0 .5rem .5rem .5rem',
-      });
+  signup(){
+    if(!this._authStateService.isLoggedIn){
+      return this._route.navigateByUrl('/signup')
+    }
+    return this._route.navigateByUrl('/business/investors-db')
+  }
   
-    gridOptions: GridOptions = {
-      pagination: true,
-      theme: this.theme,
-      onGridReady: (params) => {
-        this.gridApi = params.api;
-      },
-      columnDefs: [
-        { field: 'id', hide: true, sort: 'desc' },
-        { field: 'name',},
-        { field: 'type',},
-        {
-          field: 'minFunding',
-          valueFormatter: (params: ValueFormatterParams) => {
-            return '$' + formatCurrency(params.value);
-          },
-        },
-        {
-          field: 'maxFunding',
-          valueFormatter: (params: ValueFormatterParams) => {
-            return '$' + formatCurrency(params.value);
-          },
-        },
-        { field: 'countries' },
-        { field: 'fundingVehicle',},
-        { field: 'useOfFunds' },
-        { field: 'esgFocusAreas' },
-        { field: 'businessGrowthStages' },
-        { field: 'investmentStructures' },
-        { field: 'contactName',  },
-        { field: 'contactEmail' },
-        { field: 'website', },
-        { field: 'sectors' },
-        { field: 'subSectors' },
-        { field: 'investees' },
-        { field: 'description' },
-      ] as ColDef[],
-      defaultColDef: {
-        filter: true,
-      } as ColDef,
-    };
 }
