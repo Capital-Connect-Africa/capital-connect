e5454427c8dbd258ab19bf0991a3ee029b847456
meltus-bwire <meltusbwire10@gmail.com>
2025-03-16 23:45:06 +0300

    chore: created a kanban view of the deals

--- src/app/features/investor/pages/deals/deals.component.ts
+++ src/app/features/investor/pages/deals/deals.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DealsPipelinesStore } from '../../../deals-pipeline/store/deals.pipelines.store';
+import { CommonModule } from '@angular/common';
+import { Deal } from '../../../deals-pipeline/interfaces/deal.interface';
 
 @Component({
   selector: 'app-deals',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './deals.component.html',
   styleUrl: './deals.component.scss'
 })
 export class DealsComponent {
 
+  store =inject(DealsPipelinesStore);
+
+  getTotalDealsValue(deals:Deal[]){
+    return deals.reduce((prev, curr) => prev +curr.value, 0)
+  }
 }
