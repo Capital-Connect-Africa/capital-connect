1770241b81ccf655c8af86283b24bd458490617f
meltus-bwire <meltusbwire10@gmail.com>
2025-03-18 11:14:43 +0300

    chore: configure custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'custom-sans': ['Helvetica Neue', 'Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
