3f490dbf8e4d993d17a64755f233795f8e7c9a4a
meltus-bwire <meltusbwire10@gmail.com>
2025-03-20 09:46:18 +0300

    chore: added an event emmiter for a selected deal

--- src/app/features/deals-pipeline/services/child.events.service.ts
+++ src/app/features/deals-pipeline/services/child.events.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import {  Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChildEventsService {
+  private onDealSelected =new Subject<'Write'| 'Read'>();
   private onDealStageChange = new Subject<void>();
+
+  dealSelected$ =this.onDealSelected.asObservable();
   dealStageChange$ = this.onDealStageChange.asObservable();
+
 
   emitDealStageChangeEvent() {
     this.onDealStageChange.next();
+  }
+
+  emitDealSelectedEvent(mode: 'Write' | 'Read' ='Read'){
+    this.onDealSelected.next(mode);
   }
 }
