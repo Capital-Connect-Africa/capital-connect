5d5727e250a0c35f3d4d9c980fb137e7b029e40b
=meltus-bwire <meltusbwire10@gmail.com>
2025-04-30 11:10:19 +0300

    chore: modified the q string

--- src/app/core/services/investors/public-investors-repository.service.ts
+++ src/app/core/services/investors/public-investors-repository.service.ts
@@ -48,7 +48,7 @@ export class PublicInvestorsRepositoryService extends BaseHttpService{
 
   searchInvestors(payload:Partial<UserSearch>){
     return this.create(`${BASE_URL}/investors-repository/search`, payload).pipe(map(res =>{
-      return res as {q: string, investors:PublicInvestor[]}
+      return res as {q: string, investors:PublicInvestor[], matches: number, total: number}
     }))
   }
 
